9d5346a18accb4505671d5139956e2084de979e9
Heimer Ahnfelt <thekingofspades@live.se>
2019-04-10 22:41:40 +0200

    Added basic player movement

--- Lost Dungeon/Assets/Scripts/Managers/GameManager.cs
+++ Lost Dungeon/Assets/Scripts/Managers/GameManager.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
-
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null;
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
--- Lost Dungeon/Assets/Scripts/Player.cs
+++ Lost Dungeon/Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float horizontal;
+    private float vertical;
+    public float SpeedMultiplier = 10;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * SpeedMultiplier, Input.GetAxisRaw("Vertical") * SpeedMultiplier);
     }
 }
