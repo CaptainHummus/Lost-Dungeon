38da88546bbaa81c7629fb8a8464fa4d85c1aae8
Heimer Ahnfelt <thekingofspades@live.se>
2019-04-30 15:09:27 +0200

    More UI update and combat Canvas Base

--- Lost Dungeon/Assets/My Scripts/LoadLevelButton.cs
+++ Lost Dungeon/Assets/My Scripts/LoadLevelButton.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour
+public class LoadLevelButton : MonoBehaviour
 {
     public void GMLoadScene(string sceneName)
     {
